6fdb4a45d07744356419a44d30b423d8459b83a6
mkavcic <mkavcic1@etf.unsa.ba>
2018-12-25 18:17:24 +0100

    Prepravke na tutorijalu

--- src/ba/unsa/etf/rpr/tutorijal10/GradoviController.java
+++ src/ba/unsa/etf/rpr/tutorijal10/GradoviController.java
@@ -6,13 +6,21 @@ import javafx.scene.control.Menu;
 import javafx.scene.control.MenuItem;
 import net.sf.jasperreports.engine.JRException;
 
-
+import java.util.ResourceBundle;
 
 
 public class GradoviController {
-    private  GeografijaDAO model;
+    private GeografijaDAO model;
     @FXML
     public MenuItem printBtn;
+    public MenuItem english;
+    public MenuItem french;
+    public MenuItem bosnian;
+    public MenuItem german;
+    public Menu file;
+    public Menu help;
+    public Menu view;
+    public MenuItem jezik;
 
     public GradoviController(GeografijaDAO model) {
         this.model = model;
@@ -25,5 +33,34 @@ public class GradoviController {
         } catch (JRException e1) {
             e1.printStackTrace();
         }
+    }
+
+    public void promijeni(ActionEvent actionEvent) {
+        String property = "property";
+        MenuItem menuItem = (MenuItem) actionEvent.getSource();
+        String id = menuItem.getId();
+        switch (id) {
+            case "english":
+                property = "Translate_en_GB";
+                break;
+            case "french":
+                property = "Translate_fr";
+                break;
+            case "german":
+                property = "Translate_de";
+                break;
+            case "bosnian":
+                property = "Translate_bs_BA";
+                break;
+        }
+        file.setText(ResourceBundle.getBundle(property).getString("file"));
+        help.setText(ResourceBundle.getBundle(property).getString("help"));
+        view.setText(ResourceBundle.getBundle(property).getString("view"));
+        jezik.setText(ResourceBundle.getBundle(property).getString("Language"));
+        printBtn.setText(ResourceBundle.getBundle(property).getString("izvjestaj"));
+        bosnian.setText(ResourceBundle.getBundle(property).getString("bosanski"));
+        english.setText(ResourceBundle.getBundle(property).getString("engleski"));
+        french.setText(ResourceBundle.getBundle(property).getString("francuski"));
+        german.setText(ResourceBundle.getBundle(property).getString("njemacki"));
     }
 }
